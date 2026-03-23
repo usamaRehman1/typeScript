1ffe2f0e12a535f72ae24bde0320a1d0d34d09c9
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2026-03-23 22:35:34 +0500

    type annotation

---
+++ dataTypes/typeAnnotation.ts
@@ -0,0 +1,10 @@
+// ========== TYPE ANNOTATION (Explicitly => Mannualy define the type) ============
+
+let annotation_name: string = "muhammad usama";
+let annotation_age: number = 28;
+
+
+function add(a: number, b: number): number {
+    return a + b
+}
+console.log(add(10, 5))
